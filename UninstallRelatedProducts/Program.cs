784cd7cecb9609bca61a36cd7d35a5f25a2dc391
dwickern <dwickern@s4isystems.com>
2012-12-19 10:13:59 -0800

    Add command-line arguments
    The following arguments are available:
    --upgradecode <guid>
    --quiet

--- UninstallRelatedProducts/Program.cs
+++ UninstallRelatedProducts/Program.cs
@@ -4,25 +4,49 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
+using CommandLine;
+using CommandLine.Text;
 
 namespace UninstallRelatedProducts
 {
     class Program
     {
+        class Options : CommandLineOptionsBase
+        {
+            [Option(null, "upgradecode", Required = true, HelpText = "Upgrade code of the products to uninstall.")]
+            public string UpgradeCode { get; set; }
+
+            [Option(null, "quiet", DefaultValue = false, HelpText = "Quiet mode, no user interaction")]
+            public bool Silent { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                return HelpText.AutoBuild(this, c => HelpText.DefaultParsingErrorsHandler(this, c));
+            }
+        }
+
         static void Main(string[] args)
         {
             using (var sw = File.CreateText(@"C:\uninstall.txt"))
             {
-                sw.WriteLine("Args: " + args.Aggregate(string.Empty, (x, y) => x + y, _ => _));
                 try
                 {
-                    var upgradeCode = Guid.Parse("6b124a3c-f3f9-480d-82f9-881e0b5663a1");
+                    var options = new Options();
+                    if (!CommandLineParser.Default.ParseArguments(args, options))
+                    {
+                        // Failed to parse arguments
+                        // Arguments help text is already printed to stdout
+                        Environment.Exit(-1);
+                    }
+
+                    var upgradeCode = Guid.Parse(options.UpgradeCode);
                     var productCodes = Msi.GetRelatedProducts(upgradeCode);
                     foreach (var product in productCodes)
                     {
                         sw.WriteLine("Uninstalling: " + product);
                         Console.WriteLine(product);
-                        Msi.Uninstall(product, silent: true);
+                        Msi.Uninstall(product, options.Silent);
                     }
                 }
                 catch (Exception e)
