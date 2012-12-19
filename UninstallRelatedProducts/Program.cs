b663e4634c9716e0ce0667b8fa699e1e9341f000
dwickern <dwickern@s4isystems.com>
2012-12-19 15:04:55 -0800

    Move "Options" class to its own file

--- UninstallRelatedProducts/Program.cs
+++ UninstallRelatedProducts/Program.cs
@@ -4,41 +4,11 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using CommandLine;
-using CommandLine.Text;
 
 namespace UninstallRelatedProducts
 {
     class Program
     {
-        class Options : CommandLineOptionsBase
-        {
-            [Option(null, "upgradecode", Required = true, HelpText = "Upgrade code of the products to uninstall.")]
-            public string UpgradeCode { get; set; }
-
-            [Option(null, "peruseronly", DefaultValue = false,
-                HelpText = "Whether to only uninstall per-user products. " +
-                    "If not specified, both per-user and per-machine products will be uninstalled.")]
-            public bool PerUserOnly { get; set; }
-
-            [Option(null, "maxversion",
-                HelpText = "Maximum product version to uninstall. " +
-                    "Products with a version greater than this number will be skipped. " +
-                    "If not specified, all related products will be uninstalled.")]
-            public string MaxVersion { get; set; }
-
-            [Option(null, "log", HelpText = "Path to a log file to write progress information.")]
-            public string LogFilePath { get; set; }
-
-            [Option(null, "quiet", DefaultValue = false, HelpText = "Quiet mode, no user interaction")]
-            public bool Silent { get; set; }
-
-            [HelpOption]
-            public string GetUsage()
-            {
-                return HelpText.AutoBuild(this, c => HelpText.DefaultParsingErrorsHandler(this, c));
-            }
-        }
-
         static void Main(string[] args)
         {
             var options = new Options();
